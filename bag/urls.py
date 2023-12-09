9a8b7e7427ffb63ab46bc2d34d8754915f3a8cb0
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-12-09 11:38:29 +0000

    Added add_bag_items view

--- bag/urls.py
+++ bag/urls.py
@@ -5,4 +5,5 @@ from . import views
 
 urlpatterns = [
     path('', views.renderbag, name='bag'),
+    path('add/<item_id>/', views.add_bag_items, name='add_items'),
 ]
--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -15,3 +15,27 @@ def renderbag(request):
     }
 
     return render(request, template, context)
+
+
+def add_bag_items(request, item_id):
+    """
+        Handles the process of adding items to the
+        users shopping bag for checkout
+    """
+
+    quantity = int(request.POST.get('quantity'))
+    redirection = request.POST.get('redirect_url')
+    bag = request.session.get('bag', {})
+
+    """
+        If the item exists in the bags session
+        Then the items will be added to the bag
+    """
+    if item_id in list(bag.keys()):
+        bag[item_id] += quantity
+    else:
+        bag[item_id] = quantity
+
+    request.session['bag'] = bag
+    print(request.session['bag'])
+    return redirect(redirection)
