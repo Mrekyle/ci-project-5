c66e4d0ffe103dec764d06b11a91e8e6e2ef3588
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-12-16 10:31:47 +0000

    Added order forms forms.py file

---
+++ checkout/forms.py
@@ -0,0 +1,49 @@
+from django import forms
+from .models import Order
+
+
+class OrderForm(forms.ModelForm):
+    """
+        Generates an order form specific to the required
+        information
+    """
+
+    class Meta:
+        model = Order
+        fields = ('full_name', 'email', 'phone_number', 'street_address1',
+                  ' street_address2', 'town_or_city', 'postcode', 'country', 'county')
+
+    def __init__(self, *args, **kwargs):
+        """
+            Adds placeholders and classes to the form
+            for a better UI experience
+
+            Removes automatically generated labels
+        """
+        super().__init__(*args, **kwargs)
+
+        placeholders = {
+            'full_name': 'Fill Name',
+            'email': 'Email Address',
+            'phone_number': 'Phone Number',
+            'street_address1': 'Address Line 1',
+            'street_address2': 'Address Line 2',
+            'town_or_city': 'Town or City',
+            'county': 'County',
+            'country': 'Country',
+            'postcode': 'Zip/Postcode'
+        }
+
+        self.fields['full_name'].widget.attrs['autofocus'] = True
+
+        """
+            Setting the placeholders for the forms inputs fields
+        """
+        for field in self.fields:
+            if self.fields[field].required:
+                placeholder = f'{placeholders[field]} *'
+            else:
+                placeholder = placeholders[field]
+            self.fields[field].widget.attrs['placeholder'] = placeholder
+            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
+            self.fields[field].label = False
