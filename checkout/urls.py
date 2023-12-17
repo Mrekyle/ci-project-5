a6bdc67733242fee3f6cc5ceb66d1bc81224016a
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-12-17 12:29:03 +0000

    Added webhook handlers setup. Once deployed endpoints will be added

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 
 urlpatterns = [
     path('', views.rendercheckout, name='checkout'),
     path('checkout_success/<order_number>',
          views.rendercheckout_success, name='checkout_success'),
+    path('wh/', webhook, name='webhook'),
 ]
---
+++ checkout/webhook_handler.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+
+
+class StripeWH_Handler:
+    """
+        Stripe webhook handler
+    """
+
+    def __init__(self, request):
+        self.request = request
+
+    def handle_event(self, event):
+        """
+            Handle the generic, unknown and unexpected
+            webhook events
+        """
+
+        return HttpResponse(
+            content=f'Unhandled Webhook Received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_succeeded(self, event):
+        """
+            Handle the payment intent succeeded. After the
+            payment was successful 
+        """
+
+        return HttpResponse(
+            content=f'Webhook Received: {event["type"]}',
+            status=200)
+
+    def handle_payment_intent_payment_failed(self, event):
+        """
+            Handle the payment intent fail webhook from stripe.
+            If the payment has failed
+        """
+
+        return HttpResponse(
+            content=f'Webhook Received: {event["type"]}',
+            status=200)
