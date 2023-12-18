d830acca8a5203c99661651fc21725d01d67af3b
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-12-18 13:46:59 +0000

    Added stylung for checkout success page

--- profiles/views.py
+++ profiles/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .models import UserProfile
+
 # Create your views here.
 
 
@@ -8,9 +10,11 @@ def renderprofile(request):
         Renders the profile page of the application
     """
 
+    profile = UserProfile
+
     template = 'profile.html'
     context = {
-
+        'user': profile,
     }
 
     return render(request, template, context)
