19607cdf1d4ea87d25bcbfe533cb94224cf90c2d
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-12-20 13:52:41 +0000

    Added Delivery information. Added orders to profile page

--- profiles/urls.py
+++ profiles/urls.py
@@ -6,5 +6,8 @@ from . import views
 urlpatterns = [
     path('', views.renderprofile, name='profile'),
     path('admin-dashboard/', views.renderadmin, name='admin'),
-    path('delivery-information/', views.renderdelivery, name='delivery-info'),
+    path('delivery-information/', views.renderdelivery, name='delivery_info'),
+    path('order-history/', views.renderfullhistory, name='order_history'),
+    path('order-history/',
+         views.renderorderhistory, name='order_confirmation'),
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -12,6 +12,9 @@ from checkout.models import Order
 def renderprofile(request):
     """
         Renders the profile page of the application
+
+        Giving the user quick access to account settings, 
+        support, order history quick look and order history overview
     """
 
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -22,7 +25,7 @@ def renderprofile(request):
     context = {
         'user': profile,
         'form': form,
-        'orders': orders,
+        'order': orders,
     }
 
     return render(request, template, context)
@@ -31,6 +34,9 @@ def renderprofile(request):
 def renderdelivery(request):
     """
         Renders the users default delivery information
+
+        Allowing them to update the default for use in
+        a quicker smoother checkout
     """
 
     profile = get_object_or_404(UserProfile, user=request.user)
@@ -53,6 +59,44 @@ def renderdelivery(request):
         'form': form,
         'order': orders,
         'on_delivery': True,
+    }
+
+    return render(request, template, context)
+
+
+def renderorderhistory(request, order_number):
+    """
+        Renders the individual orders history
+    """
+
+    order = get_object_or_404(Order, order_number=order_number)
+
+    messages.info(request, f"This is a past confirmation for your order {order_number}. \
+                 A confirmation email was sent of the date it was ordered.")
+
+    template = 'checkout/checkout_success.html'
+
+    context = {
+        'order': order,
+        'from_profile': True
+    }
+
+    return render(request, template, context)
+
+
+def renderfullhistory(request):
+    """
+        Renders the users full order history in one place
+    """
+
+    profile = get_object_or_404(UserProfile, user=request.user)
+    orders = profile.orders.all()
+
+    template = 'full_history.html'
+
+    context = {
+        'user': profile,
+        'orders': orders,
     }
 
     return render(request, template, context)
