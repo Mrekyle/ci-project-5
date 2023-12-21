e6652226ac89adbb05523fae05c06d2f701b0f75
Kyle Chart <64297454+Mrekyle@users.noreply.github.com>
2023-12-21 15:43:56 +0000

    Add custom widget. Added views for edit, create and delete products. Added functionality and forms for creating new products.

--- products/urls.py
+++ products/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.rendershop, name='shop'),
     path('<int:product_id>/', views.renderproductdetail, name='product_detail'),
+    path('new/', views.rendernewproduct, name='add_product'),
+    path('edit/<int:product_id>/', views.renderedit, name='edit_product'),
+    path('delete/<int:product_id>/', views.del_product, name='delete_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,10 +1,12 @@
-from django.shortcuts import render, reverse, redirect, get_object_or_404
+from django.shortcuts import render, reverse, redirect, get_object_or_404, reverse
 from django.contrib import messages
 from django.db.models import Q
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 from django.db.models.functions import Lower
 from .models import Product, Category
+from .forms import ProductForm
 
 # Create your views here.
 
@@ -98,3 +100,89 @@ def renderproductdetail(request, product_id):
     }
 
     return render(request, template, context)
+
+
+@login_required
+def rendernewproduct(request):
+    """
+        Renders the product creation page
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, f'Sorry you dont have access to this page. \
+                       If you believe it is a mistake, please contact us.')
+        return redirect(reverse('home'))
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES)
+        if request.method == 'POST':
+            product = form.save()
+            messages.success(
+                request, f'Product {product.name} Successfully created.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Oops, something has gone wrong. Please try again later. \
+                        If the problem persists, please contact support')
+    else:
+        form = ProductForm()
+
+    form = ProductForm()
+    template = 'add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
+
+
+@login_required
+def renderedit(request, product_id):
+    """
+        Renders the edit product page of the app
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, f'Sorry you dont have access to this page. \
+                       If you believe it is a mistake, please contact us.')
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    if request.method == 'POST':
+        form = ProductForm(request.POST, request.FILES, instance=product)
+        if form.is_valid():
+            form.save()
+            messages.success(
+                request, f'Product {product.name} Successfully Edited.')
+            return redirect(reverse('product_detail', args=[product.id]))
+        else:
+            messages.error(request, f'Oops, something has gone wrong. Please try again later. \
+                        If the problem persists, please contact support')
+    else:
+        form = ProductForm(instance=product)
+        messages.info(request, f'You are currently editing {product.name}.')
+
+    template = 'edit_product.html'
+    context = {
+        'form': form,
+        'product': product,
+    }
+
+    return render(request, template, context)
+
+
+def del_product(request, product_id):
+    """
+        Handles the delete methods for a product on the store
+    """
+
+    if not request.user.is_superuser:
+        messages.error(request, f"Sorry you dont have access to be here. \
+                       Only store owners can do that!")
+        return redirect(reverse('home'))
+
+    product = get_object_or_404(Product, pk=product_id)
+    product.delete()
+    messages.success(request, f'{product.name} Has been successfully deleted.')
+
+    return redirect(reverse('product_managment'))
