b9e07950c8ae383ced93d3353fc894ec71ebd989
Mrekyle <>
2024-02-07 18:50:56 +0000

    Edit settings

--- localitty/settings.py
+++ localitty/settings.py
@@ -29,12 +29,14 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if 'DEVELOPMENT' in os.environ:
-    DEBUG = True
-    ALLOWED_HOSTS = ['*']
-else:
-    DEBUG = False
-    ALLOWED_HOSTS = ['*']
+DEBUG = True
+
+# if 'DEVELOPMENT' in os.environ:
+#     DEBUG = True
+#     ALLOWED_HOSTS = ['*']
+# else:
+#     DEBUG = False
+#     ALLOWED_HOSTS = ['*']
 
 
 # Application definition
